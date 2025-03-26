c3d56dbe63d11681acfd02183ea03c40a51e986b
George-25 <georgemallory26@gmail.com>
2025-03-26 00:42:20 -0400

    bootstrap button testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+/* The following line can be included in your src/index.js or App.js file */
+
+import { Button } from "react-bootstrap";
 
 const App = () => {
   return (
     <div>
-      <h1>Components play with Bootstrap UI library.</h1>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
     </div>
   );
 };
