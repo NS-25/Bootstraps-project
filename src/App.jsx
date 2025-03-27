5bb92baf1eabe700f6b6d557d0cca51d4e38b3fb
George-25 <georgemallory26@gmail.com>
2025-03-26 23:33:37 -0400

    created object for accordion

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,14 @@ const App = () => {
       title: "Section 1",
       content: "Content for section 1.",
     },
+    {
+      title: "Section 2",
+      content: "Content for section 2.",
+    },
+    {
+      title: "Section 3",
+      content: "Content for section 3.",
+    },
   ];
   return (
     <>
