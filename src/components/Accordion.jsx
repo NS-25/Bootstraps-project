351e93b291ae8d14312e7a02bc8ff39d0c1b63f8
George-25 <georgemallory26@gmail.com>
2025-03-27 00:00:30 -0400

    added some codes

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+// import { useState } from "react";
 
-const Accordion = () => {
-  return <div></div>;
+const Accordion = ({ items }) => {
+  // const [openItem, setOpenItem] = useState(false);
+
+  return (
+    <div className="accordion" id="accordionExample">
+      {items.map((item, index) => {
+        <div className="accordion-item" key={index}>
+          <h2 className="accordion-header">{item.title}</h2>
+          <div className="accordion-body">{item.content}</div>
+        </div>;
+      })}
+    </div>
+  );
 };
 
 export default Accordion;
