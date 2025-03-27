2119d615ff7ed4747ac734cfe026a3bebfb25769
George-25 <georgemallory26@gmail.com>
2025-03-26 23:31:29 -0400

    added some codes in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 const App = () => {
+  const accordionData = [
+    {
+      title: "Section 1",
+      content: "Content for section 1.",
+    },
+  ];
   return (
-    <div>
-      <Button as="a" variant="primary">
-        Button as link
-      </Button>
-      <Button as="a" variant="success">
-        Button as link
-      </Button>
-    </div>
+    <>
+      <div>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+      </div>
+      {/* ------Accodion--------- */}
+      <div className="container">
+        <h1>Accordion Example</h1>
+      </div>
+    </>
   );
 };
 
