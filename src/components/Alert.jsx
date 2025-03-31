ac13d2cde52586d4e46ce56b527d001bd599a3c2
George-25 <georgemallory26@gmail.com>
2025-03-30 23:39:53 -0400

    added another div for testing

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Alert = ({ variant, head }) => {
+const Alert = ({ variant, head, close }) => {
   return (
     <>
       <div className={` alert alert-${variant} `}>
         <h4 className={`alert-${head}`}>Well done</h4>
         <p>A simple success alert—check it out!</p>
+      </div>
+      <div className={` alert alert-${variant} `}>
+        <strong>Holy guacamole!</strong> You should check in on some of those
+        fields below.
+        <button type="button" className={`btn-${close}`}></button>
       </div>
     </>
   );
