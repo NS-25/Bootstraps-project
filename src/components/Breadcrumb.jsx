a55e11ce056f63562347176d77929200fbe75c3f
George-25 <georgemallory26@gmail.com>
2025-03-31 23:28:47 -0400

    added some css in breadcrum

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-const Breadcrumb = () => {
-  return <div>
-    <nav>
-      <ol>
-        <li>Home</li>
-      </ol>
-    </nav>
-  </div>;
+const Breadcrumb = ({ type }) => {
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className={`breadcrumb-${type}`}>
+            <a href="#">Home</a>
+          </li>
+        </ol>
+      </nav>
+    </div>
+  );
 };
 
 export default Breadcrumb;
