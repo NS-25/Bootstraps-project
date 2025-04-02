5bf745c8b1db90d52d5513c602846e5f24b35379
George-25 <georgemallory26@gmail.com>
2025-04-01 23:41:11 -0400

    added some props into Card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-const Card = () => {
+const Card = ({ title, imageUrl, text, buttonText}) => {
   return (
     <div>
       <img src={imageUrl} className="card-img-top" alt={title} />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <p>{text}</p>
-        <button>{buttonText}</button>
+        <p className="card-text">{text}</p>
+        <button className="btn btn-primary">{buttonText}</button>
       </div>
     </div>
   );
