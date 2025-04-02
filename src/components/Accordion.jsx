b3525b7a3844f89fe521d3034ded258de3f06501
George-25 <georgemallory26@gmail.com>
2025-04-01 20:05:49 -0400

    created accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,21 +1,11 @@
-// import React from "react";
+import React from "react";
 
-// const Accordion = ({ items }) => {
-//   return (
-//     <div className="accordion" id="accordionExample">
-//       {items.map((item, index) => (
-//         <div className="accordion-item" key={index}>
-//           <h2 className="accordion-header">
-//             {/* <button className="accordion-button" type="button">
-//               {item.title}
-//             </button> */}
-//             {item.title}
-//           </h2>
-//           <div className="accordion-body">{item.content}</div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+const Accordion = () => {
+  return (
+    <div>
+      <h1>I am according! </h1>
+    </div>
+  );
+};
 
-// export default Accordion;
+export default Accordion;
