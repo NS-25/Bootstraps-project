0b30b4c357b204e788119f15d459ae35636d6f41
George-25 <georgemallory26@gmail.com>
2025-04-09 20:39:40 -0400

    changed some codes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { useState } from "react";
 
 const Modal = () => {
@@ -10,47 +10,43 @@ const Modal = () => {
   const handleClose = () => {
     setShowModal(false);
   };
-  return ReactDOM.createPortal(
+  return (
     // <!-- Button trigger modal -->
     <>
       <button type="button" className="btn btn-primary" onClick={handleClick}>
         Launch demo modal
       </button>
-
       {/* <!-- Modal --> */}
-
-      {showModal &&
-        ((
-          <div className="modal show fade d-block" tabIndex="-1">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5">Modal title</h1>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    onClick={handleClose}
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="modal-body">I am modal component!</div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={handleClose}
-                  >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary">
-                    Save changes
-                  </button>
-                </div>
+      {showModal && (
+        <div className="modal show fade d-block" tabIndex="-1">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5">Modal title</h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={handleClose}
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">I am modal component!</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
               </div>
             </div>
           </div>
-        ),
-        document.querySelector(".modal-container"))}
+        </div>
+      )}
     </>
   );
 };
