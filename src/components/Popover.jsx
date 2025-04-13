72951ca51a58d38e8fb9ed6c77d13487449bed9c
George-25 <georgemallory26@gmail.com>
2025-04-12 23:01:43 -0400

    added some className

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -3,10 +3,18 @@ import React from "react";
 const Popover = () => {
   return (
     <div>
-      <button>Popover on top!</button>
-      <button>Popover on right!</button>
-      <button>Popover on bottom!</button>
-      <button>Popover on left!</button>
+      <button type="button" className="btn btn-secondary">
+        Popover on top!
+      </button>
+      <button type="button" className="btn btn-primary">
+        Popover on right!
+      </button>
+      <button type="button" className="btn btn-success">
+        Popover on bottom!
+      </button>
+      <button type="button" className="btn btn-info">
+        Popover on left!
+      </button>
     </div>
   );
 };
