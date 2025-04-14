4e6a642d2b9f1a320cb591339e00e5aa67c87fff
George-25 <georgemallory26@gmail.com>
2025-04-13 22:13:44 -0400

    clear

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -1,60 +1,41 @@
-import React from "react";
-import { useEffect, useRef } from "react";
-import { Popover as BootstrapPopover } from "bootstrap";
-import { usePopover } from "../hooks/PopoverContex";
-import { usePopover } from "react";
+  import React from "react";
 
-const Popover = ({
-  id,
-  label,
-  content,
-  placement = "top",
-  variant = "secondary",
-}) => {
-  const buttonRef = useRef(null);
-  const { activePopoverId, showPopover, hidePopover } = usePopover();
+  
+  import { usePopover } from "../hooks/PopoverContex";
+  import { usePopover } from "react";
 
-  useEffect(() => {
-    //////
-    if (buttonRef.current) {
-      const popover = new BootstrapPopover(buttonRef.current, {
-        trigger: "manual",
-        placement,
-        content,
-      });
-      ///////////
+  const Popover = ({
+    id,
+    label,
+    content,
+    placement = "top",
+    variant = "secondary",
+  }) => {
+    const { activePopoverId, showPopover, hidePopover } = usePopover();
+
+    
+
+    //////////
+
+    const handleClick = () => {
       if (activePopoverId === id) {
-        popover.show();
+        hidePopover();
       } else {
-        popover.hide();
+        showPopover(id);
       }
+    };
 
-      return () => popover.dispose();
-    }
-  }, [activePopoverId]);
-
-  //////////
-
-  const handleClick = () => {
-    if (activePopoverId === id) {
-      hidePopover();
-    } else {
-      showPopover(id);
-    }
+    return (
+      <div className="">
+        <button
+          type="button"
+          className={`btn btn-${variant} m-2`}
+          onClick={handleClick}
+        >
+          {label}
+        </button>
+      </div>
+    );
   };
 
-  return (
-    <div className="">
-      <button
-        type="button"
-        className={`btn btn-${variant} m-2`}
-        ref={buttonRef}
-        onClick={handleClick}
-      >
-        {label}
-      </button>
-    </div>
-  );
-};
-
-export default Popover;
+  export default Popover;
