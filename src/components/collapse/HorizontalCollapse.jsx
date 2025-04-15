22831e1a5c54622afccc46af78caf5cb4f3b01bd
George-25 <georgemallory26@gmail.com>
2025-04-15 00:18:47 -0400

    added horizontal collapse

--- src/components/collapse/HorizontalCollapse.jsx
+++ src/components/collapse/HorizontalCollapse.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import {useCollapse} from "../../hooks/CollapseProvider"
+import React from "react";
+import { useCollapse } from "../../hooks/CollapseProvider";
 
+const HorizontalCollapse = ({ id, buttonLabel, content }) => {
+  const { visibleId, toggleCollapse } = useCollapse();
+  const isOpen = visibleId === id;
 
-
-const HorizontalCollapse = ({id, buttonText, children}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="mb-3">
+        <button
+          className="btn btn-primary mb-2"
+          type="button"
+          onClick={() => toggleCollapse(id)}
+        >
+          {buttonLabel}
+        </button>
+        {/* ////////////// */}
+        <div className="d-flex align-items-start min-vh-25">
+          <div
+            className={`collapse collapse-horizontal ${
+              isOpen ? "show" : ""
+            } overflow-hidden`}
+          >
+            <div className={`card card-body ${isOpen ? "w-75" : "w-0"}`}>
+              {content}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HorizontalCollapse
+export default HorizontalCollapse;
