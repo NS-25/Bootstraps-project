4bbbfb56e8a6f88c780c82ef9c94905790f48eac
George-25 <georgemallory26@gmail.com>
2025-04-15 00:23:23 -0400

    collapse toggle done

--- src/components/collapse/CollapseToggle.jsx
+++ src/components/collapse/CollapseToggle.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useCollapse } from "../../hooks/CollapseProvider";
 
-const CollapseToggle = () => {
+const CollapseToggle = ({ id, buttonLabel }) => {
+  const { toggleCollapse } = useCollapse();
   return (
-    <div>
-      <h1>hello I am collapse</h1>
-    </div>
+    <button
+      className="btn btn-primary mb-2"
+      type="button"
+      onClick={() => toggleCollapse(id)}
+    >
+      {buttonLabel}
+    </button>
   );
 };
 
